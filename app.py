3ef0564322eb14076c6bd01124a6de9cadcb03dd
dalaszk <devdalas@gmail.com>
2025-08-19 23:31:36 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+
+from main import app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
